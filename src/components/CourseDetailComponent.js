b45fe3d9ba0545c07ed5870cb9c8148473f6c86e
Rifat <rifat0153@gmail.com>
2020-10-18 16:35:55 +0600

    Carousel Card Complete

--- src/components/CourseDetailComponent.js
+++ src/components/CourseDetailComponent.js
@@ -4,49 +4,49 @@ import { Link } from 'react-router-dom';
 import { Table } from 'reactstrap';
 
 class CourseTable extends Component {
-    constructor(props) {
-       super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-       this.state = { //state is by default an object
-          courses: COURSES
-       }
-    }   
+   constructor(props) {
+      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+      this.state = { //state is by default an object
+         courses: COURSES
+      }
+   }   
 
-    renderTableData() {
-        return this.state.courses.map((course, index) => {
-            
-           const { id, code, name, featured, description} = course //destructuring
-           return (
-              <tr key={id}>
-                 <td>{id}</td>
-                 <td>{code}</td>
-                 <td>{name}</td>
+   renderTableData() {
+      return this.state.courses.map((course, index) => {
+         
+         const { id, code, name, featured, description} = course //destructuring
+         return (
+            <tr key={id}>
+               <td>{id}</td>
+               <td>{code}</td>
+               <td>{name}</td>
 
-                 <td>{description}</td>
-              </tr>
-           )
-        })
-     }
+               <td>{description}</td>
+            </tr>
+         )
+      })
+   }
 
-     renderTableHeader() {
-        let header = Object.keys(this.state.courses[0])
-        return header.map((key, index) => {
-           return <th key={index}>{key.toUpperCase()}</th>
-        })
-     }
+   renderTableHeader() {
+      let header = Object.keys(this.state.courses[0])
+      return header.map((key, index) => {
+         return <th key={index}>{key.toUpperCase()}</th>
+      })
+   }
 
-     render() {
-        return (
-           <div>
-              <h1 id='title'>COURSE DETAILS</h1>
-              <table responsive id='courses'>
-                 <tbody >
-                    <tr>{this.renderTableHeader()}</tr>
-                    {this.renderTableData()}
-                 </tbody>
-              </table>
-           </div>
-        )
-     }
-  }
+   render() {
+      return (
+         <div>
+            <h1 id='title'>COURSE DETAILS</h1>
+            <table responsive id='courses'>
+               <tbody >
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+         </div>
+      )
+   }
+}
   
   export default CourseTable;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "react-slick";
+import "../App.css";
+import Cristiano from "../images/cristiano.jpg";
+import Messi from "../images/messi.jpg";
+import Neymar from "../images/neymar.jpg";
+import Kaka from "../images/kaka.jpg";
+import Pele from "../images/pele.jpg";
 
-// function RenderCard({item}) {
+export default class Home extends React.Component {
+  render() {
+    var settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 2 
+    };
+    return (
+      <div className="container">
+        
 
-//     return(
-//         <Card>
-//             <CardImg src={item.image} alt={item.name} />
-//             <CardBody>
-//             <CardTitle>{item.name}</CardTitle>
-//             {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-//             <CardText>{item.description}</CardText>
-//             </CardBody>
-//         </Card>
-//     );
+        <div  className="row carousel"> 
+          <div className="col-12 col-md-5 "> 
+            <h1 className="carousel-text"><b>BEST PLATFROM <br></br>TO LEARN<br></br> EVERYTHING</b></h1>
+          </div >
 
-// }
+          <div className="col-md-6"> 
+            <Slider {...settings}>
+              <div>
+                <img className="carousel-image" src={Cristiano}/>
+              </div>
+              <div>
+                <img className="carousel-image" src={Messi} />
+              </div>
+              <div>
+                <img className="carousel-image" src={Neymar} />
+              </div>
+              <div>
+                <img className="carousel-image" src={Kaka} />
+              </div>
+              <div>
+                <img className="carousel-image" src={Pele} />
+              </div>
+            </Slider>
+          </div>
 
-function Home(props) {
-    return(
-        <div className="container">
-            <div className="row align-content-center">
-                <text>This is Home</text>
-            </div>
-            
         </div>
+        
+      </div>
     );
+  }
 }
-
-export default Home;
